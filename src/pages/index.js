b25eed69e06854dcc1906ec266a623765fceb32e
husniddin <husniddinzoirjonov@gmail.com>
2023-12-23 19:32:31 +0500

    created Brands Section and Next step will be media query

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,19 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import HeroComponent from '@/components/Hero'
+import Brands from '@/components/Brands'
+import RecommendationProducts from '@/components/RecommendationProducts'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-     <HeroComponent/>
+   
+     {/* <HeroComponent/> */}
+      <Brands/>
+      <RecommendationProducts/>
+
     </>
   )
 }
