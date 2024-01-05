895a3ea7a9fcdc4a1803a03d298355abc4535424
Admeral2000 <husniddinzoirjonov@gmail.com>
2024-01-06 02:19:36 +0500

    created and made responsive cheapProducts section

--- src/components/CheapProducts/index.js
+++ src/components/CheapProducts/index.js
@@ -1,16 +1,15 @@
 import axios from "axios";
-import sty from "./index.module.css";
+import cls from "../Brands/brands.module.css";
+import stl from "../RecommendationProducts/RecommendationProducts.module.css";
+import style from "./cheapProducts.module.css";
 import { useEffect, useState } from "react";
-// import { HardIcon } from "../svg";
+import { LikeLogo, ChartLogo, BucketLogo, ArrowLogo, Percentage } from "../svg";
+import Link from "next/link";
 
-//https://api.brandstore.uz/api/home 200 so'm
-//type: "max_price_half_million 500so'm
-// type: "max_price_million 1 mln
-
-export default function CheapProduct() {
-  const [two, setData] = useState([]);
-  const [five, setFive] = useState([]);
-  const [oneMln, setOneMln] = useState([]);
+export default function RecommendationProducts() {
+  const [firstData, setFirstData] = useState([]);
+  const [secondData, setSecondData] = useState([]);
+  const [thirdData, setThirdData] = useState([]);
   const [tab, setTab] = useState(1);
 
   useEffect(() => {
@@ -22,7 +21,7 @@ export default function CheapProduct() {
       })
       .then((r) => {
         console.log(r.data.data, "Backand");
-        setData(r.data.data.product_request);
+        setFirstData(r.data.data.product_request);
       });
     axios
       .get("https://api.brandstore.uz/api/home", {
@@ -32,7 +31,7 @@ export default function CheapProduct() {
       })
       .then((r) => {
         // console.log(r.data.data, "res");
-        setFive(r.data.data.product_request);
+        setSecondData(r.data.data.product_request);
       });
     axios
       .get("https://api.brandstore.uz/api/home", {
@@ -42,194 +41,253 @@ export default function CheapProduct() {
       })
       .then((r) => {
         // console.log(r.data.data, "res");
-        setOneMln(r.data.data.product_request);
+        setThirdData(r.data.data.product_request);
       });
   }, []);
 
   return (
     <>
-      <div className={sty.cheapProduct_wrapper}>
-        <div className={sty.cheapProduct_all_item}>
-          <div className={sty.cheapProduct_title}>
-            <div className={sty.cheapProcuct_round_prt}>
-              <p>%</p>
-            </div>
-            <div className={sty.cheapProduct_title_item}>
-              <span>Товары</span>
-              <p>дешевле</p>
-            </div>
-            <div className={sty.cheapProduct_price_lvl}>
-              <button
-                id={sty.cheapProduct_tab}
-                onClick={() => setTab(1)}
-                className={tab == 1 && `actice`}
+      <section className={`${cls.sectionBrands} ${stl.sectionRecommend}`}>
+        <div className="container">
+          <div
+            className={`${cls.brandAndReccomendNavPart} ${style.cheapProductsNavbar}`}
+          >
+            <div className={cls.sectionName}>
+              <div
+                className={`${cls.yourBrandSectionLogo} ${style.percentageLogo}`}
               >
-                200 000 сум
-              </button>
-              <button
-                id={sty.cheapProduct_tab}
-                onClick={() => setTab(2)}
-                className={tab == 2 && `actice`}
-              >
-                500 000 сум
-              </button>
-              <button
-                id={sty.cheapProduct_tab}
-                onClick={() => setTab(3)}
-                className={tab == 3 && `actice`}
-              >
-                1 000 000 сум
-              </button>
-              <button className={sty.cheapProduct_all_btn}>Смотреть всё</button>
+                <Percentage />
+              </div>
+
+              <span>
+                <p>Товары</p>
+                <p>дешевле</p>
+              </span>
             </div>
+
+            {/* !!!!<<<<<<<<<<<<     Price   >>>>>>>>>>>> */}
+
+            <ul className={style.price}>
+              <li>
+                <button
+                  id={style.cheapProduct_tab}
+                  onClick={() => setTab(1)}
+                  className={tab == 1 && `${style.buttonActive}`}
+                >
+                  200 000 сум
+                </button>
+              </li>
+
+              <li>
+                <button
+                  id={style.cheapProduct_tab}
+                  onClick={() => setTab(2)}
+                  className={tab == 2 && `${style.buttonActive}`}
+                >
+                  500 000 сум
+                </button>
+              </li>
+
+              <li>
+                <button
+                  id={style.cheapProduct_tab}
+                  onClick={() => setTab(3)}
+                  className={tab == 3 && `${style.buttonActive}`}
+                >
+                  1 000 000 сум
+                </button>
+              </li>
+            </ul>
+
+            <button>Смотреть всё</button>
           </div>
-          <div className={sty.cheapProduct_api_map_wrap}>
-            <div>
-              <div className={sty.cheapProduct_tab}>
-                {tab == 1 && (
-                  <div className={sty.cheapProduct_container}>
-                    {two.length &&
-                      two.slice(0, 6).map((item) => (
-                        <div
-                          className={sty.cheapProcuct_two_wrap}
-                          key={item.id}
-                        >
-                          <div className={sty.cheap_img}>
-                            <div className={sty.heapProduc_hard_icon}>
-                              {/* <HardIcon /> */}
-                            </div>
-                            <img
-                              className={sty.cheapProduct_img}
-                              src={item.images[0].url}
-                            />
-                            <p className={sty.cheapProduct_item_name}>
-                              {item.random_shop.name}
-                            </p>
-                            <p className={sty.cheapProduct_brand_name}>
+
+          <div className={stl.recommendationProductsWrapper}>
+            {/* <<<<<<<<<<<<<<Cost 200 000>>>>>>>>>>>>>>>> */}
+
+            {tab == 1 && (
+              <ul
+                className={`${stl.recommendationProducts} ${style.cheapProducts}`}
+              >
+                {firstData.length &&
+                  firstData.slice(0, 6).map((item) => (
+                    <li
+                      key={item.id}
+                      className={stl.recommendationProductsItem}
+                    >
+                      <Link href="#">
+                        <div className={stl.likeLogo}>
+                          <LikeLogo /> <ChartLogo />
+                        </div>
+                        <div className={stl.recommendationProductsImage}>
+                          <img src={item.images[0].url} alt={item.name} />
+                        </div>
+                        <div className={stl.recommendProductInfo}>
+                          <div className={stl.recommendProductTitle}>
+                            <p>{item.class.name}</p>
+                            <p className={style.cheapProductsName}>
                               {item.name}
                             </p>
-                            <div className={sty.cheapProduct_item_price}>
-                              <div>
-                                <span className={sty.span}>
-                                  {item.random_shop.monthly_price} so'm / oy
-                                </span>
-                                <p className={sty.cheapProduct_price}>
-                                  {item.random_shop.price} So'm
-                                </p>
-                              </div>
-                              <p className={sty.cheapProduct_thereIs}>
-                                <p className={sty.cheapProduct_thereIs_text}>
-                                  Mavjud:
-                                </p>
-                                {item.random_shop.bs_quant}-dona
-                              </p>
-                              <button>Qo'shish</button>
+                          </div>
+                          <div className={stl.recommendProductQuantity}>
+                            <p>Доступно:</p>
+                            <p>{item.random_shop.quantity} шт.</p>
+                          </div>
+                          <div className={stl.recommendProductPrice}>
+                            <div>
+                              <p>{item.random_shop.monthly_price} сум / мес.</p>
+                              <p>{item.random_shop.price} сум</p>
                             </div>
+                            <button>
+                              <span hidden="hidden">
+                                <BucketLogo />
+                              </span>
+                              в Корзину
+                            </button>
                           </div>
                         </div>
-                      ))}
-                  </div>
-                )}
-              </div>
-              <div className={sty.cheapProduct_tab}>
-                {tab == 2 && (
-                  <div className={sty.cheapProduct_container}>
-                    {five.length &&
-                      five.slice(0, 6).map((item) => (
-                        <div
-                          className={sty.cheapProcuct_two_wrap}
-                          key={item.id}
-                        >
-                          <div className={sty.cheap_img}>
-                            <div className={sty.heapProduc_hard_icon}>
-                              <HardIcon />
-                            </div>
-                            <img
-                              className={sty.cheapProduct_img}
-                              src={item.images[0].url}
-                            />
-                            <p className={sty.cheapProduct_item_name}>
-                              {item.random_shop.name}
-                            </p>
-                            <p className={sty.cheapProduct_brand_name}>
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            )}
+
+            {/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Cost 500 000 >>>>>>>>>>> */}
+
+            {tab == 2 && (
+              <ul
+                className={`${stl.recommendationProducts} ${style.cheapProducts}`}
+              >
+                {secondData.length &&
+                  secondData.slice(0, 6).map((item) => (
+                    <li
+                      key={item.id}
+                      className={stl.recommendationProductsItem}
+                    >
+                      <Link href="#">
+                        <div className={stl.likeLogo}>
+                          <LikeLogo /> <ChartLogo />
+                        </div>
+                        <div className={stl.recommendationProductsImage}>
+                          <img src={item.images[0].url} alt={item.name} />
+                        </div>
+                        <div className={stl.recommendProductInfo}>
+                          <div className={stl.recommendProductTitle}>
+                            <p>{item.class.name}</p>
+                            <p className={style.cheapProductsName}>
                               {item.name}
                             </p>
-                            <div className={sty.cheapProduct_item_price}>
-                              <div>
-                                <span className={sty.span}>
-                                  {item.random_shop.monthly_price} so'm / oy
-                                </span>
-                                <p>{item.random_shop.price} So'm</p>
-                              </div>
-                              <p className={sty.cheapProduct_thereIs}>
-                                <p className={sty.cheapProduct_thereIs_text}>
-                                  Mavjud:
-                                </p>
-                                {item.random_shop.bs_quant}-dona
-                              </p>
-
-                              <button>Qo'shish</button>
+                          </div>
+                          <div className={stl.recommendProductQuantity}>
+                            <p>Доступно:</p>
+                            <p>{item.random_shop.quantity} шт.</p>
+                          </div>
+                          <div className={stl.recommendProductPrice}>
+                            <div>
+                              <p>{item.random_shop.monthly_price} сум / мес.</p>
+                              <p>{item.random_shop.price} сум</p>
                             </div>
+                            <button>
+                              <span hidden="hidden">
+                                <BucketLogo />
+                              </span>
+                              в Корзину
+                            </button>
                           </div>
                         </div>
-                      ))}
-                  </div>
-                )}
-              </div>
-              <div className={sty.cheapProduct_tab}>
-                {tab == 3 && (
-                  <div className={sty.cheapProduct_container}>
-                    {oneMln.length &&
-                      oneMln.slice(0, 6).map((item) => (
-                        <div
-                          className={sty.cheapProcuct_two_wrap}
-                          key={item.id}
-                        >
-                          <div className={sty.cheap_img}>
-                            <div className={sty.heapProduc_hard_icon}>
-                              <HardIcon />
-                            </div>
-                            <img
-                              className={sty.cheapProduct_img}
-                              src={item.images[0].url}
-                            />
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            )}
 
-                            <p className={sty.cheapProduct_item_name}>
-                              {item.random_shop.name}
-                            </p>
-                            <p className={sty.cheapProduct_brand_name}>
+            {/* <<<<<<<<<<<<<<<<<<<Cost 1 000 000>>>>>>>>>>>>>>>>> */}
+
+            {tab == 3 && (
+              <ul
+                className={`${stl.recommendationProducts} ${style.cheapProducts}`}
+              >
+                {thirdData.length &&
+                  thirdData.slice(0, 6).map((item) => (
+                    <li
+                      key={item.id}
+                      className={stl.recommendationProductsItem}
+                    >
+                      <Link href="#">
+                        <div className={stl.likeLogo}>
+                          <LikeLogo /> <ChartLogo />
+                        </div>
+                        <div className={stl.recommendationProductsImage}>
+                          <img src={item.images[0].url} alt={item.name} />
+                        </div>
+                        <div className={stl.recommendProductInfo}>
+                          <div className={stl.recommendProductTitle}>
+                            <p>{item.class.name}</p>
+                            <p className={style.cheapProductsName}>
                               {item.name}
                             </p>
-                            <div className={sty.cheapProduct_item_price}>
-                              <div>
-                                <span className={sty.span}>
-                                  {item.random_shop.monthly_price} so'm / oy
-                                </span>
-                                <p>{item.random_shop.price} So'm</p>
-                              </div>
-                              <p className={sty.cheapProduct_thereIs}>
-                                <p className={sty.cheapProduct_thereIs_text}>
-                                  Mavjud:
-                                </p>
-                                {item.random_shop.bs_quant}-dona
-                              </p>
-
-                              <button>Qo'shish</button>
+                          </div>
+                          <div className={stl.recommendProductQuantity}>
+                            <p>Доступно:</p>
+                            <p>{item.random_shop.quantity} шт.</p>
+                          </div>
+                          <div className={stl.recommendProductPrice}>
+                            <div>
+                              <p>{item.random_shop.monthly_price} сум / мес.</p>
+                              <p>{item.random_shop.price} сум</p>
                             </div>
+                            <button>
+                              <span hidden="hidden">
+                                <BucketLogo />
+                              </span>
+                              в Корзину
+                            </button>
                           </div>
                         </div>
-                      ))}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            )}
+
+            <div className={stl.recommendProductsSectionAds}>
+              <div>
+                <Link href="#">
+                  <div className={stl.recommendProductsSectionAdsInfo}>
+                    <p>
+                      VIVO <span>X550</span>
+                    </p>
+                    <span>
+                      <ArrowLogo />
+                    </span>
+                    <p>
+                      А также стремящиеся вытеснить традиционное производство,
+                      технология в целые кластеры себе подобных.
+                    </p>
                   </div>
-                )}
+                </Link>
               </div>
-            </div>
-            <div className={sty.cheapProduct_adverd}>
-              <div className={sty.cheapProduct_adverd_forst}></div>
-              <div className={sty.cheapProduct_adverd_secand}></div>
+
+              <div>
+                <Link href="#">
+                  <div className={stl.recommendProductsSectionAdsInfo}>
+                    <p>
+                      HUAWEI <span>P55</span>
+                    </p>
+                    <span>
+                      <ArrowLogo />
+                    </span>
+                    <p>
+                      А также стремящиеся вытеснить традиционное производство,
+                      технология в целые кластеры себе подобных.
+                    </p>
+                  </div>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
